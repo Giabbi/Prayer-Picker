c743a786cc763f27f241526ad97ec6b4bea101c4
Giancarlo Umberto Ambrosino <129298577+Giabbi@users.noreply.github.com>
2024-04-24 13:45:14 -0700

    Update students.js

--- students.js
+++ students.js
@@ -8,12 +8,36 @@ $w.onReady(function () {
 		if (returntype == ' ') wixLocation.to('/')
 		else session.setItem('class',returntype)
 	})
-
+	
 	let subject = session.getItem('class')
+	if (subject == null) $w('#section1').hide()
+	$w('#text4').text = "Students in " + subject
 	queryStudents(subject)
-	.then ((returntype)=>{
+	.then((results)=>{
 		$w('#repeater1').onItemReady(($item, itemData) => {
 			$item('#name').text = itemData.name
 		})
+		$w('#repeater1').data = results
 	})
-})
+});
+
+export function random_click(event) {
+	let subject = session.getItem("class")
+	selectStudent(subject)
+	.then((results) => {
+		$w('#random').hide()
+		$w('#resultText').text = "The student who says the prayer today is " + results
+		session.setItem("pick", results)
+		$w('#resultText').show()
+	})
+}
+
+export function absent_click(event) {
+	let student = session.getItem("pick")
+	markAbsent(student)
+	let subject = session.getItem("class")
+	selectStudent(subject)
+	.then((results) => {
+		$w('#resultText').text = "The new student is " + results
+	})
+}
