68b96b3edb2fbedfd45647e6bdea3dbdb90bd5c1
Giancarlo Umberto Ambrosino <129298577+Giabbi@users.noreply.github.com>
2024-04-26 11:38:41 -0700

    Backend function for picker page implemented
    See comments for additional information

--- backend.js
+++ backend.js
@@ -40,3 +40,123 @@ export function isvalid (id){
     else return false
   })
 }
+export function queryStudents(subject) {
+    return wixData.query('Students')
+    .eq ('schedule',subject)
+    .find (options)
+    .then ((results)=>{
+        return results.items
+    })
+}
+
+export function getDailyPrayer() {
+  const url = 'https://www.churchofengland.org/prayer-and-worship/join-us-service-daily-prayer/todays-prayer';
+  
+  return fetch(url)
+    .then(response => response.text())
+    .then(body => {
+      const $ = cheerio.load(body);
+      const dailyPrayer = $('#dailyprayer .inner p').first().text(); 
+      return dailyPrayer;
+    })
+    .catch(err => {
+      console.error('Failed to fetch daily prayer:', err);
+      return 'Error fetching prayer.';
+    });
+}
+
+export function selectStudent(subject) {
+    return wixData.query("Students")
+    .eq("schedule", subject)
+    .find(options)
+    .then((results) => {
+        let newList = []
+        let notAbsent = []
+        for(let i = 0; i < results.items.length; i++) {
+            if(!results.items[i]["hasSaidPrayer"] && !results.items[i]["isAbsent"]) newList.push(results.items[i])
+            if(!results.items[i]["isAbsent"]) notAbsent.push(results.items[i])
+        }
+        let pick = 0
+        if(newList.length == 0) {
+            return resetHasSaidPrayer(results.items)
+                .then((updates) => {
+                    console.log(updates)
+                    let pick = crypto.randomInt(0, results.items.length);
+                    let student = notAbsent[pick]
+                    let buffer = {
+                        "_id": student["_id"],
+                        "name": student["name"],
+                        "schedule": student["schedule"],
+                        "hasSaidPrayer": true,
+                        "isAbsent": false
+                    }
+                    return wixData.update("Students", buffer, options)
+                    .then(() => {
+                        return student["name"]
+                    })
+                });
+        }
+        else {
+            pick = crypto.randomInt(0, newList.length)
+            let student = newList[pick]
+            let buffer = {
+                "_id": student["_id"],
+                "name": student["name"],
+                "schedule": student["schedule"],
+                "hasSaidPrayer": true,
+                "isAbsent": false
+            }
+            return wixData.update("Students", buffer, options)
+            .then(() => {
+                return student["name"]
+            })
+        }
+    })
+}
+
+export function resetHasSaidPrayer(students) {
+    const updates = students.map(student => {
+        return wixData.update("Students", {
+            "_id": student._id,
+            "name": student.name,
+            "schedule": student.schedule,
+            "hasSaidPrayer": false,
+            "isAbsent": student.isAbsent
+        }, options);
+    });
+    return Promise.all(updates);
+}
+
+export function markAbsent(student) {
+    console.log(student)
+    return wixData.query("Students")
+    .eq("name", student)
+    .find(options)
+    .then((results) => {
+        let buffer = {
+            "_id": results.items[0]["_id"],
+            "name": results.items[0]["name"],
+            "schedule": results.items[0]["schedule"],
+            "hasSaidPrayer": false,
+            "isAbsent": true
+        }
+        return wixData.update("Students", buffer, options)
+    })
+}
+
+export function resetAbsence() {
+    return wixData.query("Students")
+    .find(options)
+    .then((results) => {
+        const updates = results.items.map(student => {
+        return wixData.update("Students", {
+            "_id": student._id,
+            "name": student.name,
+            "schedule": student.schedule,
+            "hasSaidPrayer": student.hasSaidPrayer,
+            "isAbsent": false
+        }, options);
+    });
+    return Promise.all(updates);
+    })
+}
