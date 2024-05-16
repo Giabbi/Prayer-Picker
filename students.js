929bc2557a11f6a03ffdb005d6112802561cca9b
Giancarlo Umberto Ambrosino <129298577+Giabbi@users.noreply.github.com>
2024-05-15 22:34:49 -0700

    Fixed absence bug and comments

--- students.js
+++ students.js
@@ -1,30 +1,33 @@
-import {isvalid,queryStudents, selectStudent, markAbsent, getDailyPrayer} from 'backend/backend.jsw';
+import {isvalid,queryStudents, selectStudent, markAbsent, getDailyPrayer, getPrayer} from 'backend/backend.jsw';
 import {session} from 'wix-storage-frontend';
 import wixLocation from 'wix-location';
+
 $w.onReady(function () {
 	let id = session.getItem('id')
-	isvalid(id)
-	.then ((returntype)=>{
-		if (returntype == ' ') wixLocation.to('/')
-		else session.setItem('class',returntype)
-	})
-	
-	let subject = session.getItem('class')
-	if (subject == null) $w('#section1').hide()
-	$w('#text4').text = "Students in " + subject
-	queryStudents(subject)
-	.then((results)=>{
-		populateRepeater(results)
+	isValid(id) // Check if user is logged in
+	.then ((results)=>{
+		if (results == ' ') wixLocation.to('/')
+		else {
+			session.setItem('class', results)
+			$w('#text4').text = "Students in " + results
+			queryStudents(results)
+			.then((results1)=>{
+				getPeriod(results1)
+				.then((period) => {
+					populateRepeater(results1, period) // Gets students from database and populate repeater
+				})
+			})
+		}
 	})
 });
 
-export function populateRepeater(results) {
+export function populateRepeater(results, period) {
 	$w('#repeater1').onItemReady(($item, itemData) => {
 		$item('#name').text = itemData.name
-		if(itemData.hasSaidPrayer) $item("#hasSaidPrayerText").text = itemData.name +" has already said the prayer"
-		else $item("#hasSaidPrayerText").text = itemData.name +" has not said the prayer yet"
-		if(itemData.isAbsent) $item("#isAbsentText").text = itemData.name + " was marked absent for this period"
-		else $item("#isAbsentText").text = itemData.name + " is marked as present for this period"
+		if(itemData.hasSaidPrayer[period]) $item("#hasSaidPrayerText").text = "Has already said the prayer"
+		else $item("#hasSaidPrayerText").text = "Has not said the prayer yet"
+		if(itemData.isAbsent) $item("#isAbsentText").text = "Student is marked as Absent"
+		else $item("#isAbsentText").text = "Student is marked as Here"
 	})
 	$w('#repeater1').data = results
 	$w('#repeater1').show()
@@ -33,7 +36,7 @@ export function populateRepeater(results) {
 
 export function random_click(event) {
 	let subject = session.getItem("class")
-	selectStudent(subject)
+	selectStudent(subject) // Gets random student from backend
 	.then((results) => {
 		$w('#random').hide()
 		$w('#resultText').text = "The student who says the prayer today is " + results
@@ -41,35 +44,38 @@ export function random_click(event) {
 		$w('#absent').show()
 		
 		$w('#text8').show()
-		getDailyPrayer()
+		getDailyPrayer() // Scrape daily prayer (function in backend)
 		.then((prayer) => {
 			$w('#text6').show()
 			$w('#prayer').text = prayer
 			$w('#prayer').show()
 			$w('#text8').hide()
 		})
-		queryStudents(subject)
+		queryStudents(subject) // Update the repeater
 		.then((students) => {
 			populateRepeater(students)
 		})
 	})
 }
 
-export function absent_click(event) {
+export function absent_click(event) { // Re-roll student in case he/she is absent
 	$w('#absent').disable()
 	$w('#text9').show()
 	let student = $w('#resultText').text.slice(41) 
-	markAbsent(student)
-	.then(() => {
-		let subject = session.getItem("class")
-		selectStudent(subject)
-		.then((results) => {
-			$w('#resultText').text = "The student who says the prayer today is " + results
-			$w('#absent').enable()
-			$w('#text9').hide()
-			queryStudents(subject)
-			.then((students) => {
-				populateRepeater(students)
+	let subject = session.getItem("class")
+	getPeriod(subject)
+	.then((period)=> {
+		markAbsent(student, period)
+		.then(() => {		
+			selectStudent(subject)
+			.then((results) => {
+				$w('#resultText').text = "The student who says the prayer today is " + results
+				$w('#absent').enable()
+				$w('#text9').hide()
+				queryStudents(subject)
+				.then((students) => {
+					populateRepeater(students, period)
+				})
 			})
 		})
 	})
